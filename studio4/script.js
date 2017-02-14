062e5ca456ecb58ae1b7700e59ef8e57d746a658
shanglu9425 <shanglu9425ll@hotmail.com>
2017-02-14 03:41:21 -0800

    update

--- studio4/script.js
+++ studio4/script.js
@@ -39,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
             var calm = document.getElementById('modal-gif');
             calm.classList.add("hatchAnimation");
             console.log("fdsafsdaf");
-        } else if (newScrollPosition > 600) {
+        } else if (newScrollPosition > 520) {
             var body = document.getElementById('pageTwo');
             body.classList.add("slideLeft");
             console.log("123");
