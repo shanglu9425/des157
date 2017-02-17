fe9956c0235c66c3f5ddb3deac6f3c03af2cb916
shanglu9425 <shanglu9425ll@hotmail.com>
2017-02-17 00:21:55 -0800

    added soung visualization

---
+++ studio5/musicscript.js
@@ -0,0 +1,64 @@
+$(document).ready(function() {
+    var audioCtx = new(window.AudioContext || window.webkitAudioContext)();
+    var audioElement = document.getElementById('audioElement');
+    var audioSrc = audioCtx.createMediaElementSource(audioElement);
+    var analyser = audioCtx.createAnalyser();
+
+    // Bind our analyser to the media element source.
+    audioSrc.connect(analyser);
+    audioSrc.connect(audioCtx.destination);
+    var frequencyData = new Uint8Array(55);
+
+    var svgHeight = '300';
+    var svgWidth = '600';
+    var barPadding = '6';
+    var paddingSide = '50';
+
+    function createSvg(parent, height, width) {
+        return d3.select(parent).append('svg').attr('height', height).attr('width', width);
+    }
+
+    var svg = createSvg(' .wave', svgHeight, svgWidth);
+
+    // Create our initial D3 chart.
+    svg.selectAll('rect')
+        .data(frequencyData)
+        .enter()
+        .append('rect')
+        .attr('x', function(d, i) {
+            return i * (svgWidth / frequencyData.length);
+        })
+        .attr('width', svgWidth / frequencyData.length - barPadding);
+
+    function renderChart() {
+        requestAnimationFrame(renderChart);
+
+        // Copy frequency data to frequencyData array.
+        analyser.getByteFrequencyData(frequencyData);
+
+        // Update d3 chart with new data.
+        svg.selectAll('rect')
+            .data(frequencyData)
+            .attr('y', function(d) {
+                return svgHeight - d;
+            })
+            .attr('height', function(d) {
+                return d;
+            })
+            .attr('fill', function(d) {
+                return 'rgb(255, 255, 255)';
+            });
+    }
+
+    // Run the loop
+    renderChart();
+
+    $(function() {
+        $('#start').on('click', function() {
+            alert('hello');
+            $(this).addClass('classOne');
+        });
+    });
+
+
+});
