e561363f7035cda365eea8e09b5e769283f8578f
shanglu9425 <shanglu9425ll@hotmail.com>
2017-03-20 21:57:22 -0700

    update

--- studio6/script.js
+++ studio6/script.js
@@ -24,6 +24,40 @@ function fDate(){
 
 fDate();
 
+
+//counter score 
+//Get element for the counter
+var counter = $('#counter');
+//Run count function
+count(counter, 0, 30, 1000);
+
+//Count function - element for counter, starting count number, ending count number, time in milliseconds
+function count(elem, startnum, endnum, time){
+  
+  //Create a variable to store the current counter number and add it to the counter
+  var curnum = startnum;
+  elem.text(curnum);
+  
+  //Work out the speed of the counter
+  var speed = time / (endnum - startnum);
+  
+  //Create the counter animation
+  var timer = window.setInterval(function(){
+    
+    //Test if counter has finished
+    if(curnum < endnum){
+      //Increase the counter by 1 and add it to the counter
+      curnum++;
+      elem.text("Score" + ": " + curnum);
+    }else{
+      //Stop the animation
+      clearInterval(timer);
+    }
+    
+  },speed);
+  
+}
+
   //today date 
 /*var today = new Date();
 var dd = today.getDate();
